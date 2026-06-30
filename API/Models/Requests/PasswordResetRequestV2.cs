4747252aefa1906e332aba99884c0622880eea53
Kulturnilpferd <kulturnilpferd@webl.de>
2026-06-30 23:39:06 +0200

    Fix legacy WebUI compatibility

--- API/Models/Requests/PasswordResetRequestV2.cs
+++ API/Models/Requests/PasswordResetRequestV2.cs
@@ -8,7 +8,6 @@ public sealed class PasswordResetRequestV2
     [EmailAddress(true)]
     public required string Email { get; set; }
     
-    [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = false)]
     [System.ComponentModel.DataAnnotations.StringLength(HardLimits.MaxTurnstileResponseTokenLength)]
-    public required string TurnstileResponse { get; set; }
+    public string? TurnstileResponse { get; set; }
 }
